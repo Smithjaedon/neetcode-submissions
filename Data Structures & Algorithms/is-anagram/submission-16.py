f56e26b2f27c0d83046495afd988a3462e2b52be
Jaedon Smith <jaedon.andres@gmail.com>
2026-06-19 07:58:18 -0400

    Add: is-anagram - submission-16

---
+++ Data Structures & Algorithms/is-anagram/submission-16.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        seen = {}
+        if len(s) != len(t):
+            return False
+        for c in s:
+            seen[c] = seen.get(c, 0) + 1
+        for c in t:
+            if c not in seen:
+                return False
+            seen[c] -= 1
+            if seen[c] < 0:
+                return False
+        
+        return True
