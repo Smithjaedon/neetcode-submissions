bdbbcc08af85d8e2fb05641098e186219d91cc49
Jaedon Smith <jaedon.andres@gmail.com>
2026-06-30 19:38:20 -0400

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = {}
+        res = []
+        arr = []
+        for i, s in enumerate(strs):
+            SORT = "".join(sorted(s))
+            if SORT not in seen:
+                seen[SORT] = [s]
+            else:
+                seen[SORT].append(s)
+        return [c for c in seen.values()]
